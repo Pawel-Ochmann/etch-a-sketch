11d19a82f62cc2bc6dc4f6e2c6b420e3caaa0019
Pawel Ochmann <pawelochmann@poczta.onet.pl>
2023-03-28 14:39:09 +0200

    Function that creates grid of divs working, change event listener - now function is fired only when 'mouseDown' variable returns true

--- main.js
+++ main.js
@@ -1,8 +1,32 @@
 const container = document.querySelector('.container');
-let div = document.createElement('div');
-div.style.width = '960px';
-div.style.height = '960px';
-div.addEventListener('mouseenter', (e)=> {
-    div.style.backgroundColor = "black"
+const body = document.querySelector('body');
+
+let mouseDown = false;
+
+body.addEventListener('mousedown', ()=> {
+    mouseDown = true;
 });
-container.appendChild(div);
+body.addEventListener('mouseup', ()=> {
+    mouseDown = false;
+})
+
+
+function createGrid(number) {
+    let squareNumber = number*number;
+    for (let i = 0; i<squareNumber; i++) {
+        let div = document.createElement('div');
+        
+        div.style.width = `${Math.floor(container.clientWidth/number)}px`;
+        div.style.height = `${Math.floor(container.clientHeight/number)}px`;
+        
+
+        div.addEventListener('mouseenter', (e)=> {
+        if (mouseDown) {
+            div.style.backgroundColor = "black"
+            }
+        });
+        container.appendChild(div);
+    }
+}
+
+createGrid(16);
